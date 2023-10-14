e832982afac6894fe05f36ff74c37a67eef2a63b
AnonymousAcid <anonymousacid14@gmail.com>
2023-10-14 12:36:38 -0700

    preparing for manual controls

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 using namespace vex;
 
 // A global instance of competition
-competition Competition;
+// competition Competition;
 
 vex::brain       Brain;
 
@@ -21,7 +21,7 @@ controller Controller1 = controller(primary);
 motor LeftMotor = motor(PORT2, ratio6_1, false);
 motor RightMotor = motor(PORT1, ratio6_1, true);
 
-drivetrain Drivetrain = drivetrain(LeftMotor, RightMotor, 320, 203, 190, vex::distanceUnits::mm, 1);
+drivetrain Drivetrain = drivetrain(LeftMotor, RightMotor, 319.19, 228.6, 190.5, mm, 1);
 
 motor Feeder = motor(PORT16, ratio18_1, false);
 motor FeederJoint = motor(PORT13, ratio36_1, false);
@@ -40,6 +40,10 @@ motor FeederJoint = motor(PORT13, ratio36_1, false);
 void pre_auton(void) {
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
+  FeederJoint.setStopping(brakeType::brake);
+  FeederJoint.setVelocity(60, percent);
+  Drivetrain.setTurnVelocity(50 , percent);
+  Drivetrain.setDriveVelocity(50, percent);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -54,20 +58,46 @@ void pre_auton(void) {
 
 void autonomous(void) {
   Brain.Screen.print("Mode: Auto");
+
+  LeftMotor.setVelocity(20, percent);
+  RightMotor.setVelocity(20, percent);
+  
+  Drivetrain.setDriveVelocity(20, percent);  
+
+  //Drivetrain controls
+  Drivetrain.driveFor(60,vex::distanceUnits::in, true);
+  Drivetrain.turnFor(-90, deg);
+
+  Drivetrain.driveFor(-6, vex::distanceUnits::in, true);
+
+  //drop the triball and take the feeder back in to not bash the feeder
+  FeederJoint.spinFor(1300, deg);
+  Feeder.spinFor(-800, deg);
+  FeederJoint.spinFor(-1300, deg);
+
+  //ram the ball under the net
+  Drivetrain.setDriveVelocity(80, percent);
+  Drivetrain.driveFor(24, vex::distanceUnits::in, true); 
   
   Drivetrain.setDriveVelocity(50, percent);
+  Drivetrain.driveFor(-6, vex::distanceUnits::in, true);
+  Drivetrain.turnFor(180, deg);
 
-  Drivetrain.driveFor(24,vex::distanceUnits::in);
-  Drivetrain.turnFor(90, vex::rotationUnits::deg);
-  Drivetrain.driveFor(60,vex::distanceUnits::in);
-  Drivetrain.turnFor(90, vex::rotationUnits::deg);
   //extrude feeder and take the triball
-  Drivetrain.driveFor(6, vex::distanceUnits::in);
-  Drivetrain.turnFor(180, vex::rotationUnits::deg);
-  //drop the triball and take the feeder back in to not bash the feeder
-  Drivetrain.setDriveVelocity(99, percent);
-  Drivetrain.driveFor(18, vex::distanceUnits::in); //ram the ball under the net
-  Drivetrain.setDriveVelocity(50, percent);
+  FeederJoint.spinFor(1300, deg);
+  Feeder.spinFor(800, deg);
+  FeederJoint.spinFor(-1300, deg);
+
+  Drivetrain.turnFor(180, deg);
+
+  //drop the triball and take the feeder back in
+  FeederJoint.spinFor(1300, deg);
+  Feeder.spinFor(-800, deg);
+  FeederJoint.spinFor(-1300, deg);
+
+  Drivetrain.setDriveVelocity(80, percent);
+  Drivetrain.driveFor(24, vex::distanceUnits::in, true); //ram the ball under the net
+  Drivetrain.setDriveVelocity(50, percent);  
 }
 
 /*---------------------------------------------------------------------------*/
@@ -82,6 +112,9 @@ void autonomous(void) {
 void usercontrol(void) {
   Brain.Screen.clearLine(1);
   Brain.Screen.print("Mode: Drive");
+
+  LeftMotor.setVelocity(20, percent);
+  RightMotor.setVelocity(20, percent);
   while (1) {
     //Drivetrain control
     int hAxis = Controller1.Axis1.position();
@@ -95,9 +128,13 @@ void usercontrol(void) {
     //  When R1 is pressed, go backward.
     //  When R2 is pressed, go forward.
     //  When both are pressed, don't move.
+    //  Hold X to spin feeder faster
+    int feederSpeed = Controller1.ButtonX.pressing() ? 200 : 80;
     int feederDirection = Controller1.ButtonR2.pressing() - Controller1.ButtonR1.pressing();
-    Feeder.setVelocity(100*feederDirection, percent);
-    int feederJointDirection = Controller1.ButtonL2.pressing() - Controller1.ButtonL1.pressing();
+    Feeder.setVelocity(feederSpeed*feederDirection, percent);
+
+    //L1 and L2 for controlling feeder joint
+    int feederJointDirection = Controller1.ButtonL1.pressing() - Controller1.ButtonL2.pressing();
     FeederJoint.setVelocity(50*feederJointDirection, percent);
     Feeder.spin(forward);
     FeederJoint.spin(forward);
@@ -113,8 +150,9 @@ void usercontrol(void) {
 int main() {
   // Set up callbacks for autonomous and driver control periods.
   // Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
-
+  // Competition.drivercontrol(usercontrol);
+  autonomous();
+  
   // Run the pre-autonomous function.
   pre_auton();
 
